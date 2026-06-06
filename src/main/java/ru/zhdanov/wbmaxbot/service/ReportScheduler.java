5009602c88f88718747fb402711dfc154810a837
dmitry <dmitryshagaev@bk.ru>
2026-06-06 18:30:22 +0700

    Skip scheduled runs outside runtime mode

--- src/main/java/ru/zhdanov/wbmaxbot/service/ReportScheduler.java
+++ src/main/java/ru/zhdanov/wbmaxbot/service/ReportScheduler.java
@@ -3,14 +3,17 @@ package ru.zhdanov.wbmaxbot.service;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import ru.zhdanov.wbmaxbot.config.AppProperties;
 
 @Component
 @ConditionalOnProperty(name = "app.scheduler.enabled", havingValue = "true", matchIfMissing = true)
 public class ReportScheduler {
 
+    private final AppProperties properties;
     private final ReportCoordinator reportCoordinator;
 
-    public ReportScheduler(ReportCoordinator reportCoordinator) {
+    public ReportScheduler(AppProperties properties, ReportCoordinator reportCoordinator) {
+        this.properties = properties;
         this.reportCoordinator = reportCoordinator;
     }
 
@@ -19,6 +22,9 @@ public class ReportScheduler {
             initialDelayString = "${app.scheduler.initial-delay}"
     )
     public void runScheduledReport() {
+        if (!"run".equalsIgnoreCase(properties.getMode())) {
+            return;
+        }
         reportCoordinator.executeScheduledRun();
     }
 }
